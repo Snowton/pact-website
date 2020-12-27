19baf3f26193f2fb346a816ae7185c6ffc4cbfc2
Shreya Mogulothu <shreya.mogulothu@gmail.com>
2020-12-26 22:10:50 -0500

    add ejs files with routes

--- app.js
+++ app.js
@@ -8,6 +8,38 @@ app.set("view engine", "ejs")
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(express.static("public"))
 
+app.get("/", (req, res) => {
+    res.render("home");
+})
+
+app.get("/instructors", (req, res) => {
+    res.render("instructors")
+})
+
+app.get("/program", (req, res) => {
+    res.render("program/faq")
+})
+
+app.get("program/overview", (req, res) => {
+    res.render("program/overview")
+})
+
+app.get("program/logistics", (req, res) => {
+    res.render("program/logistics")
+})
+
+app.get("/program/faq", (req, res) => {
+    res.render("program/faq")
+})
+
+app.get("/application", (req, res) => {
+    res.render("application")
+})
+
+app.get("/students", (req, res) => {
+    res.render("students")
+})
+
 app.listen(3000, (err) => {
     if (!err) console.log("successfully started on port 3000");
     else console.log(err);
