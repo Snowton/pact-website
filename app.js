090be88a737eeb5e0bafd796fb42b8483d50c884
Shreya Mogulothu <shreya.mogulothu@gmail.com>
2020-12-30 16:25:51 -0500

    make large again

--- app.js
+++ app.js
@@ -17,11 +17,11 @@ app.use(express.static("public"))
 const root = "https://mighty-beyond-04832.herokuapp.com"
 
 app.get("/", (req, res) => {
-    res.render("home", {url: root + req.url, img: root + "/img/home/" + "work.jpg"});
+    res.render("home", {url: root + req.url, img: root + "/img/home/" + "work.jpg", title: "PACT"});
 })
 
 app.get("/instructors", (req, res) => {
-    res.render("instructors", {lecturers: lecturers, url: root + req.url, img: root + "/img/home/" + "college.jpg"})
+    res.render("instructors", {lecturers: lecturers, url: root + req.url, img: root + "/img/home/" + "college.jpg", title: "Instructors | PACT"})
 })
 
 app.get("/program", (req, res) => {
@@ -29,23 +29,23 @@ app.get("/program", (req, res) => {
 })
 
 app.get("/program/overview", (req, res) => {
-    res.render("program/overview", {url: root + req.url, img: root + "/img/home/" + "expressions.jpg"})
+    res.render("program/overview", {url: root + req.url, img: root + "/img/home/" + "expressions.jpg", title: "Program Overview | PACT"})
 })
 
 app.get("/program/logistics", (req, res) => {
-    res.render("program/logistics", {url: root + req.url, img: root + "/img/home/" + "fun.jpg"})
+    res.render("program/logistics", {url: root + req.url, img: root + "/img/home/" + "fun.jpg", title: "Program Logistics | PACT"})
 })
 
 app.get("/program/faq", (req, res) => {
-    res.render("program/faq", {faq: faq, url: root + req.url, img: root + "/img/home/" + "lemma.jpg"})
+    res.render("program/faq", {faq: faq, url: root + req.url, img: root + "/img/home/" + "lemma.jpg", title: "Program FAQ | PACT"})
 })
 
 app.get("/application", (req, res) => {
-    res.render("application", {url: root + req.url, img: root + "/img/home/" + "modern.jpg"})
+    res.render("application", {url: root + req.url, img: root + "/img/home/" + "modern.jpg", title: "Application | PACT"})
 })
 
 app.get("/students", (req, res) => {
-    res.render("students", {group_photos, group_photos, url: root + req.url, img: root + "/img/home/" + "summer.jpg"})
+    res.render("students", {group_photos, group_photos, url: root + req.url, img: root + "/img/home/" + "summer.jpg", title: "Students | PACT"})
 })
 
 app.listen(process.env.PORT || 3000, (err) => {
