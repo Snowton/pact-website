b8730839d125e5a2e33bb8422ccb489c9e6d4a95
Shreya Mogulothu <shreya.mogulothu@gmail.com>
2020-12-30 16:14:04 -0500

    customized url and img

--- app.js
+++ app.js
@@ -14,12 +14,14 @@ app.set("view engine", "ejs")
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(express.static("public"))
 
+const root = "https://mighty-beyond-04832.herokuapp.com"
+
 app.get("/", (req, res) => {
-    res.render("home");
+    res.render("home", {url: root + req.url, img: root + "/img/home/" + "chalkboard.jpg"});
 })
 
 app.get("/instructors", (req, res) => {
-    res.render("instructors", {lecturers: lecturers})
+    res.render("instructors", {lecturers: lecturers, url: root + req.url, img: root + "/img/home/" + "college.jpg"})
 })
 
 app.get("/program", (req, res) => {
@@ -27,23 +29,23 @@ app.get("/program", (req, res) => {
 })
 
 app.get("/program/overview", (req, res) => {
-    res.render("program/overview")
+    res.render("program/overview", {url: root + req.url, img: root + "/img/home/" + "expressions.jpg"})
 })
 
 app.get("/program/logistics", (req, res) => {
-    res.render("program/logistics")
+    res.render("program/logistics", {url: root + req.url, img: root + "/img/home/" + "fun.jpg"})
 })
 
 app.get("/program/faq", (req, res) => {
-    res.render("program/faq", {faq: faq})
+    res.render("program/faq", {faq: faq, url: root + req.url, img: root + "/img/home/" + "lemma.jpg"})
 })
 
 app.get("/application", (req, res) => {
-    res.render("application")
+    res.render("application", {url: root + req.url, img: root + "/img/home/" + "modern.jpg"})
 })
 
 app.get("/students", (req, res) => {
-    res.render("students", {group_photos, group_photos})
+    res.render("students", {group_photos, group_photos, url: root + req.url, img: root + "/img/home/" + "summer.jpg"})
 })
 
 app.listen(process.env.PORT || 3000, (err) => {
