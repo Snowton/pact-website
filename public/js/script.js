0417b3bedbc43086f6740dd636788b0f55f7b893
Shreya Mogulothu <shreya.mogulothu@gmail.com>
2021-02-28 18:45:28 -0500

    brought back the JS files PHEW

---
+++ public/js/script.js
@@ -0,0 +1,49 @@
+const tog = document.getElementsByClassName("toggle");
+let i;
+
+for (i = 0; i < tog.length; i++) {
+  tog[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    let answer = this.nextElementSibling;
+    if (answer.style.maxHeight) {
+      answer.style.maxHeight = null;
+    } else {
+      answer.style.maxHeight = answer.scrollHeight + "px";
+    }
+  });
+}
+
+const group_photos = document.querySelectorAll(".group img");
+
+for (i = 0; i < group_photos.length; i++) {
+  group_photos[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    let answer = this.nextElementSibling;
+    if (answer.style.maxHeight) {
+      answer.style.maxHeight = null;
+    } else {
+      answer.style.maxHeight = answer.scrollHeight + "px";
+    }
+  });
+}
+
+const ham = document.getElementsByClassName("ham");
+ham[0].addEventListener("click", function() {
+  this.classList.toggle("active");
+  let answer = this.nextElementSibling;
+    if (answer.style.maxHeight) {
+      answer.style.maxHeight = null;
+    } else {
+      answer.style.maxHeight = answer.scrollHeight + "px";
+    }
+})
+
+// const nav_toggle = document.getElementsByClassName("toggle-nav");
+// nav_toggle[0].addEventListener("click", function() {
+//   let answer = this.nextElementSibling;
+//     if (answer.style.maxHeight) {
+//       answer.style.maxHeight = null;
+//     } else {
+//       answer.style.maxHeight = answer.scrollHeight + "px";
+//     }
+// })
